4dd82da8ea2179fdb674a44589dadc4e90044af5
itales00 <italescraftimp@gmail.com>
2024-10-22 14:05:21 -0300

    Versão inicial da aplicação Node.js

---
+++ meu-app/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('<h1>Página Inicial</h1><p>Bem-vindo à minha aplicação web!</p>');
+});
+
+app.get('/sobre', (req, res) => {
+    res.send('<h1>Sobre</h1><p>Esta é uma aplicação Node.js com múltiplas rotas.</p>');
+});
+
+app.get('/contato', (req, res) => {
+    res.send('<h1>Contato</h1><p>Entre em contato pelo email: contato@meuapp.com</p>');
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
